ce174ba87273fbdd44e13c8ec7572c65a1d31e9a
miran <armiran46@gmail.com>
2023-10-06 12:36:16 +0600

    database connection

---
+++ testweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace testweb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- testweb/Models/Category.cs
+++ testweb/Models/Category.cs
@@ -4,7 +4,6 @@ namespace testweb.Models
 {
     public class Category
     {
-        [Key]
         public int ID { get; set; }
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
